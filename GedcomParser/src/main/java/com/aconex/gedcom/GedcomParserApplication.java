cb0cf094578f3a15801c221795e2d90d0023b01f
ashas <ashas@cisco.com>
2018-01-28 20:32:37 +0530

    updating .gitignore file

--- GedcomParser/src/main/java/com/aconex/gedcom/GedcomParserApplication.java
+++ GedcomParser/src/main/java/com/aconex/gedcom/GedcomParserApplication.java
@@ -4,9 +4,9 @@ import com.aconex.gedcom.core.GedcomParser;
 
 public class GedcomParserApplication {
 
-    public static String DEFAULT_INPUT_FILE_PATH = "/Users/ashas/Applns/workspace-sts-3.7.3.RELEASE/PosterManager/GedcomParser/src/main/resources/sampleinput.txt";
+    public static String DEFAULT_INPUT_FILE_PATH = "/Users/ashas/Projects/Gedcom/GedcomParser/src/main/resources/sampleinput.txt";
 
-    public static String DEFAULT_OUTPUT_FILE_PATH = "/Users/ashas/Applns/workspace-sts-3.7.3.RELEASE/PosterManager/GedcomParser/src/main/resources/output.xml";
+    public static String DEFAULT_OUTPUT_FILE_PATH = "/Users/ashas/Projects/Gedcom/GedcomParser/src/main/resources/output.xml";
 
     public static void main(String[] args) {
         String inputFilePath = DEFAULT_INPUT_FILE_PATH;
